3ebd62bc94989f6d4b2b2715485fea269e84d526
handoiditu1508 <handoiditu1508@gmail.com>
2020-06-24 19:44:42 +0700

    19:44 2020-06-24

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+#from sklearn.cluster import KMeans
+from kmeans import KMeans
 from sklearn.datasets import make_blobs
 
 features, target = make_blobs(n_samples = 100,n_features = 2,centers = 5,cluster_std = 0.5,random_state = 10)
